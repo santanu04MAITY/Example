9f129679669880fc0831f2396541930d7252a1ca
santanu maity <santanumaityisback@gmail.com>
2022-09-28 20:40:26 +0530

    Browser Launch
    I have successfully launch the browser.

--- santanu/src/main/java/com/qa/testbase/TestBase.java
+++ santanu/src/main/java/com/qa/testbase/TestBase.java
@@ -1,9 +1,35 @@
 package com.qa.testbase;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class TestBase {
+	
+	WebDriver driver;
+	
+	@BeforeMethod
 	public void setUp()
 	{
-		
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		driver.get("http://13.126.233.235:86/");
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+	}
+	
+	@AfterMethod
+	public void tearDown()
+	{
+		driver.quit();
 	}
 
 }
